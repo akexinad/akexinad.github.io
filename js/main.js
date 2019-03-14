e2df35f5577052c81112d66c065c8bbf7247e244
danny <danixeka@gmail.com>
2019-03-14 12:23:41 +1100

    jquery scrolls

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
   console.log("hello");
 
-  $('body').addClass('fade-in')
+  $('body').addClass('fade-in');
 
+  $('#home').click(function () {
+    $('html, body').animate({
+      scrollTop: $('body').offset().top
+    }, 'slow');
+  });
 
-})
+  $('#projects').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#js-projects').offset().top
+    }, 'slow');
+  });
+
+  $('#skills').click(function () {
+    $('html, body').animate({
+      scrollTop: $(".white-container").offset().top
+    }, 'slow');
+  })
+
+});
